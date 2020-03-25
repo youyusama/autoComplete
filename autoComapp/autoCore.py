07dc3d452a3e479e635e4eea12bb6f9cd5428e3b
youyu <474323375@qq.com>
2020-03-26 01:23:23 +0800

    200326-增加ngram模型1

--- autoComapp/autoCore.py
+++ autoComapp/autoCore.py
@@ -3,8 +3,8 @@ import sys
 sys.path.append(r'D:\CNM\NTeat\autoCom\autoComapp\Autocomplete_Transformer')
 sys.path.append(r'D:\CNM\NTeat\autoCom\autoComapp\src')
 sys.path.append(r'D:\CNM\NTeat\autoCom\autoComapp')
-from autoComapp.Autocomplete_Transformer.objrun_kg import objrun_kg
-from autoComapp.Autocomplete_Transformer.objmain import objmain
+#from autoComapp.Autocomplete_Transformer.objrun_kg import objrun_kg
+#from autoComapp.Autocomplete_Transformer.objmain import objmain
 #from autoComapp.src.objgpt2 import objgpt2
 from autoComapp.objngram import objngram,NGram
 
@@ -13,19 +13,19 @@ def get_autocom(modelname):
     if modelname == 'ngram':
         if 'ngram' not in g:
             g.ngram = objngram()
-            return g.ngram
+        return g.ngram
     # elif modelname == 'gpt2':
     #     if 'gpt2' not in g:
     #         g.gpt2=objgpt2()
-    #         return g.gpt2
+    #     return g.gpt2
     # elif modelname == 'transformer':
     #     if 'transformer' not in g:
     #         g.transformer=objmain()
-    #         return g.transformer
+    #     return g.transformer
     # elif modelname == 'transformerkg':
     #     if 'transformerkg' not in g:
     #         g.transformerkg=objrun_kg()
-    #         return g.transformerkg
+    #     return g.transformerkg
 
 if __name__ == "__main__":
     obj = objngram()
